4580baa455b33fd28a1afd62a8ef5e6069b7d5a2
soutenance14 <soutenance14@gmail.com>
2021-12-02 08:11:00 +0100

    delete get all users link

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,7 +10,10 @@ use Doctrine\ORM\Mapping as ORM;
  * @ApiResource(
  *     attributes= {"security"="is_granted('ROLE_USER')"},
  *     collectionOperations={
- *         "get","post"
+ *         "get"={
+ *              "security"="is_granted('ROLE_USER') and object.getClient() == user or is_granted('ROLE_ADMIN')"
+ *          },
+ *         "post"
  *      },
  *      itemOperations={
  *      "get"={
